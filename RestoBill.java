77c6d0a7f5f82c740915d5be924f9c105421163a
Jhalak2211 <142232153+Jhalak2211@users.noreply.github.com>
2025-12-17 18:23:14 +0530

    changed

--- RestoBill.java
+++ RestoBill.java
@@ -5,16 +5,6 @@ import java.awt.event.*;
 
 public class RestoBill {
 
-    /*
-        This is a Restaurant Billing System built using Java Swing.
-        I used JCheckBox for selecting food items, JTextField for quantity input, and JTextArea to display the bill.
-        When the Generate Bill button is clicked, ActionListener checks which items are selected and calculates the total amount accordingly.
-    
-        ⭐ HOW THIS LOOKS ON RESUME
-            Project: Restaurant Billing System
-            Tech: Java, Swing, JTable, Event Handling
-            Features: Bill generation, tax calculation, input validation
-        */
     public static void main(String[] args) {
 
         JFrame frame = new JFrame("Restaurant Billing System");
